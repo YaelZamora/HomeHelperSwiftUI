8cf3c8e2880bf41a9f507b411c96597604ec33ad
YaelZamora <ajedrezkasparov@gmail.com>
2023-05-12 14:29:42 -0500

    Initial Commit

---
+++ HomeHelper/HomeHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeHelperApp.swift
+//  HomeHelper
+//
+//  Created by MacMini on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
